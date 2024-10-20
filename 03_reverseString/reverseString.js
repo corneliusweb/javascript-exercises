9927114ae85711331bb130869b6ee9397d03bd48
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-20 12:18:43 +0100

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function (str) {
+	if (str) {
+		const splitStr = str.split('');
+		const reversedStr = splitStr.reverse();
+		const joinedStr = reversedStr.join('');
 
+		return joinedStr;
+	}
+	return '';
 };
-
 // Do not edit below this line
 module.exports = reverseString;
