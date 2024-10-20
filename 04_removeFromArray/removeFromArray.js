b47fb2c4909178edddd0124f35a05206c8df49e3
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-20 12:39:52 +0100

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (mainArr, ...otherArr) {
+   const freshArray = [];
+
+   for (let i = 0; i < mainArr.length; i++) {
+      if (otherArr.includes(mainArr[i])) {
+         continue;
+      }
+      freshArray.push(mainArr[i])
+   }
+   return freshArray;
 };
 
 // Do not edit below this line
