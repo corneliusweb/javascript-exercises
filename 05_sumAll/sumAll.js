ea45ba999f8334403513cb3c1692f9dc749c8a29
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-20 13:59:40 +0100

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (n1, n2) {
+	if (n1 < 0 || n2 < 0) return 'ERROR';
+	if (Number.isInteger(n1) && Number.isInteger(n2)) {
+		let sum = 0;
+		if (n1 < n2) {
+			while (n1 <= n2) {
+				sum += n1;
+				n1++;
+			}
+			return sum;
+		} else if (n1 > n2) {
+			while (n2 <= n1) {
+				sum += n2;
+				n2++;
+			}
+			return sum;
+		}
+	}
+	return 'ERROR';
 };
 
 // Do not edit below this line
