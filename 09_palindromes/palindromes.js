23513a3cf0318026e4911fc2fd77ec74420bc061
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-31 16:35:03 +0100

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
+	const breakWord = str
+		.toLowerCase()
+		.split('')
+		.filter((words) => alphanumeric.includes(words))
+		.join('');
 
+	const reversedWord = breakWord.split('').reverse().join('');
+	return reversedWord === breakWord;
 };
 
 // Do not edit below this line
