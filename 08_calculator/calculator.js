ea29f47dab07997456a8eca15d534c20f5e74ac5
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-31 16:03:11 +0100

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,42 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	return array.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function (n1, n2) {
+	return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+   let result = num;
+
+   if (num === 0 || num === 1) return 1;
+
+   while (num > 1) {
+      num--;
+      result *= num;
+   }
+   return result;
 };
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
